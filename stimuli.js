a9fcaaab5c8d0d768f28f4f44953eedd3c06e438
anahika <anahikakapoor185041@gmail.com>
2021-05-22 17:29:34 +0530

    created experimment

---
+++ stimuli.js
@@ -0,0 +1,82 @@
+var test_stimuli= [
+  {
+    "word": "hii",
+    "lebel": "valid"
+  },
+  {
+    "word": "hello",
+    "lebel": "valid"
+  },
+  {
+    "word": "what",
+    "lebel": "valid"
+  },
+  {
+    "word": "nice",
+    "lebel": "valid"
+  },
+  {
+    "word": "cool",
+    "lebel": "valid"
+  },
+  {
+    "word": "good",
+    "lebel": "valid"
+  },
+  {
+    "word": "happy",
+    "lebel": "valid"
+  },
+  {
+    "word": "dude",
+    "lebel": "valid"
+  },
+  {
+    "word": "damn",
+    "lebel": "valid"
+  },
+  {
+    "word": "this",
+    "lebel": "valid"
+  },
+  {
+    "word": "afs",
+    "lebel": "nonvalid"
+  },
+  {
+    "word": "asdff",
+    "lebel": "nonvalid"
+  },
+  {
+    "word": "buijn",
+    "lebel": "nonvalid"
+  },
+  {
+    "word": "nijoiu",
+    "lebel": "nonvalid"
+  },
+  {
+    "word": "jhjhu",
+    "lebel": "nonvalid"
+  },
+  {
+    "word": "kxi",
+    "lebel": "nonvalid"
+  },
+  {
+    "word": "shf",
+    "lebel": "nonvalid"
+  },
+  {
+    "word": "svjh",
+    "lebel": "nonvalid"
+  },
+  {
+    "word": "nkjhh",
+    "lebel": "nonvalid"
+  },
+  {
+    "word": "polkj",
+    "lebel": "nonvalid"
+  }
+ ]
